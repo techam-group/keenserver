34bd0716a9996832d3cc498d2b66683a3113dcc9
Favour George C <phavorsparks@gmail.com>
2019-09-23 00:55:06 +0100

    SWITCH to dotenv

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-require('./src/config/config')
+// require('./src/config/config')
+require('dotenv').config()
 const { MONGODB_URI, MONGODB_URI_OFFLINE, PORT, SSL_PORT, NODE_ENV } = process.env
 const { ApolloServer } = require('apollo-server')
 const fs = require('fs')
@@ -15,6 +16,8 @@ const { getUser } = require('./src/utils/helpers')
 
 const DB_URI = NODE_ENV ? MONGODB_URI : MONGODB_URI_OFFLINE
 new DB(superAdminDetails).connect(DB_URI)
+console.log(DB_URI)
+console.log(process.env.HEROKU_APP_NAME)
 
 const server = new ApolloServer({
   cors: true,
