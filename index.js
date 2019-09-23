c5118b246ffed42bf9d99bf1b0aa2767d693d8fd
Favour George C <phavorsparks@gmail.com>
2019-09-23 16:12:15 +0100

    checking for node env

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const dataSources = require('./src/datasources')
 
 const { getUser } = require('./src/utils/helpers')
 
-const DB_URI = NODE_ENV ? MONGODB_URI : MONGODB_URI_OFFLINE
+console.log(NODE_ENV)
+const DB_URI = NODE_ENV === 'production' ? MONGODB_URI : MONGODB_URI_OFFLINE
 new DB(superAdminDetails).connect(DB_URI)
 
 const server = new ApolloServer({
